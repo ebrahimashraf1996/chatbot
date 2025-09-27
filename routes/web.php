71598d7f048f93d54d5c2a2a501fa84e7d8812bd
Ibrahim Ashraf <ebrahimashraf1996@gmail.com>
2025-09-27 09:04:05 +0300

    Updates

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,7 @@ use Twilio\TwiML\MessagingResponse;
 Route::get('/test', function () {
     $resp = new MessagingResponse();
     $resp->message('test');
+    return response($resp, 200)
+        ->header('Content-Type', 'text/xml');
+
 });
